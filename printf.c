008f232e0dfd2bb5fe6cae3c64b553131ab3d762
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-07-22 18:25:42 +0000

    Commit of task 0

--- printf.c
+++ printf.c
@@ -12,33 +12,36 @@ int _printf(const char *format, ...)
 	int i = 0, len = 0;
 
 	va_start(list, format);
-	while (format[i] != '\0' && i <= _strlen(format) - 1)
+	if (format != NULL)
 	{
-		if (format[i] == '%')
+		while (format[i] != '\0' && i <= _strlen(format) - 1)
 		{
-			switch (format[i + 1])
+			if (format[i] == '%')
 			{
-				case 'c':
-					len += _putchar(va_arg(list, int));
-					i += 2;
-					break;
-				case 's':
-					len += _puts(va_arg(list, char *));
-					i += 2;
-					break;
-				case '%':
-					len += _putchar(format[i + 1]);
-					i += 2;
-					break;
-				default:
-					i++;
-					continue;
+				switch (format[i + 1])
+				{
+					case 'c':
+						len += _putchar(va_arg(list, int));
+						i += 2;
+						break;
+					case 's':
+						len += _puts(va_arg(list, char *));
+						i += 2;
+						break;
+					case '%':
+						len += _putchar(format[i + 1]);
+						i += 2;
+						break;
+					default:
+						i++;
+						continue;
+				}
+			}	
+			else
+			{
+				len += _putchar(format[i]);
+				i++;
 			}
-		}
-		else
-		{
-			len += _putchar(format[i]);
-			i++;
 		}
 	}
 	return (len);
