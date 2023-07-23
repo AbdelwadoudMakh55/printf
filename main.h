7c3fb3507a14f19e942e5ce3a1242c348f9a44c7
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-07-23 11:30:39 +0000

    Commit of task 0

--- main.h
+++ main.h
@@ -2,10 +2,8 @@
 #define MAIN_H
 
 int _putchar(char c);
-int _puts(char *str);
+int _puts(const char *str);
 int _strlen(const char *s);
-int print_c(char c);
-int print_s(char *string);
 int _printf(const char *format, ...);
 
 #endif
