ba4b363b3f193aacdad0976b3b0e8a67e1b43488
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-07-24 09:29:55 +0000

    Commit of task 13

--- main.h
+++ main.h
@@ -5,7 +5,7 @@ int _putchar(char c);
 int _puts(char *str);
 int _strlen(const char *s);
 int print_number(int n);
-void _print_rev_recursion(char *s);
+int _print_rev_recursion(char *s);
 int print_binary(unsigned int n);
 int _printf(const char *format, ...);
 
